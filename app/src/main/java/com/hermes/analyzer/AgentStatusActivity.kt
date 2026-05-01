775e849eca24057f54dc499c98a2ec5755221134
mangogold912-droid <mangogold912@gmail.com>
2026-05-01 16:51:13 +0900

    Fix imports for AgentStatusActivity

--- app/src/main/java/com/hermes/analyzer/AgentStatusActivity.kt
+++ app/src/main/java/com/hermes/analyzer/AgentStatusActivity.kt
@@ -3,6 +3,7 @@ package com.hermes.analyzer
 import android.graphics.Color
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.hermes.analyzer.ai.AdvancedAIEngine
